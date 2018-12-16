a22c8c29137fd33cd869c9c73bf7a020694dc23e
Jesse <jesse.bellingham@acurus.com.au>
2018-12-16 15:26:48 +1100

    Updated server to return one randomly selected comment -- added page explaination to view

--- server.js
+++ server.js
@@ -15,33 +15,43 @@ const r = new snoowrap({
 });
 
 app.get("/", function(req, res) {
-  res.render("index", { threadTitle: null, comments: null });
+  res.render("index", { threadTitle: null, comment: null, threadLink: null });
 });
 
 app.post("/", function(req, res) {
   console.log("post");
   let threadTitle = "";
+  let threadLink = "";
+
   r.getSubreddit("writingprompts")
     .getHot({ limit: 1 })
-    .filter(_ => _.stickied === false)
+    .filter(_ => !_.stickied && _.num_comments >= 5)
     .forEach(function(val) {
       threadTitle = val.title.replace("[WP]", "");
+      threadLink = val.permalink;
       console.log(val.title);
       console.log(val.comments.length);
 
       val.expandReplies({ depth: 1, limit: 1 }).then(function(sub) {
         console.log(sub.comments.length);
-        const comments = sub.comments
-          .filter(_ => _.stickied === false)
+        var comments = sub.comments
+          .filter(_ => !_.stickied && !_.removed)
           .sort(function(a, b) {
             return a.ups > b.ups;
           })
-          .map(_ => _.body_html);
-        comments.forEach(function(comment) {
-          comments.push(comment);
+          .map(function(comment) {
+            return {
+              text: comment.body_html,
+              link: comment.id,
+              author: comment.author.name
+            };
+          });
+        var comment = comments[Math.floor(Math.random() * comments.length)];
+        res.render("index", {
+          threadTitle: threadTitle,
+          comment: comment,
+          threadLink: threadLink
         });
-
-        res.render("index", { threadTitle: threadTitle, comments: comments });
       });
     });
 });
